1daf0b38da22bd2773c2ad965d1b13c837773fbc
anghelo20 <sumblech17@outlook.es>
2021-08-13 22:01:18 -0500

    new class

--- app/src/main/java/com/example/restauranteuns/Repartidor.kt
+++ app/src/main/java/com/example/restauranteuns/Repartidor.kt
@@ -3,9 +3,13 @@ package com.example.restauranteuns
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Repartidor : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun addNuew (){
+        3+4
     }
 }
