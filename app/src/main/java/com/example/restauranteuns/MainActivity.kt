e66179e373840cd6e3525336c31f0c3c88c515d3
nightmare-9871 <junior_sagitario97@hotmail.com>
2021-08-16 18:37:15 -0500

    lista_pedidos con lista desplegable

--- app/src/main/java/com/example/restauranteuns/MainActivity.kt
+++ app/src/main/java/com/example/restauranteuns/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.restauranteuns
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val spinner=findViewById<Spinner>(R.id.spinner)
+        var listo= listOf("pollo a la brasa","chaufa","lomo saltado","ceviche")
+        val adaptador=ArrayAdapter(this,android.R.layout.simple_spinner_item,listo)
+        spinner.adapter=adaptador
     }
 }
