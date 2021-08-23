e36d283c32d94f21b4a7a146eb9fb3580b832589
nightmare-9871 <junior_sagitario97@hotmail.com>
2021-08-23 16:23:33 -0500

    mi parte

--- app/src/main/java/com/example/restauranteuns/ComidasActivity.kt
+++ app/src/main/java/com/example/restauranteuns/ComidasActivity.kt
@@ -1,37 +1,31 @@
 package com.example.restauranteuns
 
-import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import android.view.View
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.ArrayAdapter
-import android.widget.EditText
 import android.widget.Spinner
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.fragment_interfaz_cliente.*
+import kotlinx.android.synthetic.main.fragment_comida.*
 
-class MainActivity : AppCompatActivity() {
+class ComidasActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val db= FirebaseFirestore.getInstance()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_interfaz_cliente)
-        var precio=findViewById<View>(R.id.costo) as EditText
-        var plato=findViewById<View>(R.id.spinner) as Spinner
-        val Chaufa=15
-        val saltado=25
-        val Ceviche=22
+        setContentView(R.layout.fragment_comida)
+
         val Cliente="fernando"
         val spinner=findViewById<Spinner>(R.id.spinner)
-        var listo= listOf("pollo a la brasa","chaufa","lomo saltado","ceviche")
+        val listo= arrayOf("pollo a la brasa","chaufa","lomo saltado","ceviche")
         var comida=0
         val adaptador=ArrayAdapter(this,android.R.layout.simple_spinner_item,listo)
         spinner.adapter=adaptador
-        var seleccion=spinner.selectedItem.toString()
+        val seleccion=spinner.selectedItem.toString()
+        print(seleccion)
+
+
         continuar.setOnClickListener{
             if(seleccion.equals("pollo a la brasa")){
                 comida=20
@@ -45,9 +39,9 @@ class MainActivity : AppCompatActivity() {
 
             }
             }
-            var resultado= comida.toInt()+costo.text.toString().toInt()
+            var resultado= comida.toInt()*costo.text.toString().toInt()
 
-                db.collection("cliente").document(seleccion).set(
+                db.collection("another").document(seleccion).set(
                     hashMapOf(
                         "cliente" to Cliente,
                         "platos" to seleccion,
@@ -55,17 +49,12 @@ class MainActivity : AppCompatActivity() {
                         "coste" to comida,
                         "total" to resultado
 
-
                     )
 
 
                 )
 
-
-
-
-
-            startActivity(Intent(this,tracking::class.java))
+            startActivity(Intent(this,TrackingActivity::class.java))
         }
         fun onContextItemSelected(item: MenuItem): Boolean {
             return super.onContextItemSelected(item)
--- app/src/main/java/com/example/restauranteuns/TrackingActivity.kt
+++ app/src/main/java/com/example/restauranteuns/TrackingActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
-class tracking : AppCompatActivity(){
+class TrackingActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_tracking)
