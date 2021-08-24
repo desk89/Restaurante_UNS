93a7f40b7c35b5e2c2e4aaa27946cd7d41e0e895
anghelo20 <sumblech17@outlook.es>
2021-08-23 19:24:37 -0500

    otra mejora

--- app/src/main/java/com/example/restauranteuns/Repartidor.kt
+++ app/src/main/java/com/example/restauranteuns/Repartidor.kt
@@ -2,14 +2,63 @@ package com.example.restauranteuns
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.*
 
 class Repartidor : AppCompatActivity() {
+
+
+    private  lateinit var recyclerView: RecyclerView
+    private  lateinit var pedArrayList: ArrayList<Pedido>
+    private  lateinit var myAdapter: MyAdapter
+    private lateinit var db: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.Repartidor)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+
+
+        pedArrayList = arrayListOf()
+
+        myAdapter = MyAdapter(pedArrayList)
+
+        recyclerView.adapter = myAdapter
+
+
+        EvenChangeListener()
     }
 
-    fun addNuew (){
-        3+4
+    private fun EvenChangeListener() {
+        db = FirebaseFirestore.getInstance()
+        db.collection("Pedidos").
+                addSnapshotListener(object : EventListener<QuerySnapshot>{
+                    override fun onEvent(
+                        value: QuerySnapshot?,
+                        error: FirebaseFirestoreException?
+                    ) {
+                        if (error != null){
+                            Log.e("Firestore Error",error.message.toString())
+                            return
+                        }
+
+                        for (dc : DocumentChange in value?.documentChanges!!){
+
+                            if (dc.type == DocumentChange.Type.ADDED){
+                                pedArrayList.add(dc.document.toObject(Pedido::class.java))
+                            }
+                        }
+
+                        myAdapter.notifyDataSetChanged()
+                    }
+
+                })
     }
+
+
 }
